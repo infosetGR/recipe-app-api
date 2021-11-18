47d51e70075d30094ce93e5c350393f77c0bb884
infosetGR <fotis@infoset.co>
2021-11-18 11:56:07 +0200

    tags and ingredients

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 # from django.db.models.constraints import UniqueConstraint
-
+from django.conf import settings
 
 class UserManager(BaseUserManager):
 
@@ -24,10 +24,33 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser,PermissionsMixin):
-        """user model with email instead of username"""
-        email = models.EmailField(max_length=255,unique=True)
-        name = models.CharField(max_length=255)
-        is_active = models.BooleanField(default=True)
-        is_staff=models.BooleanField(default=False)
-        objects = UserManager()
-        USERNAME_FIELD='email'
+    """user model with email instead of username"""
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    objects = UserManager()
+    USERNAME_FIELD='email'
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
